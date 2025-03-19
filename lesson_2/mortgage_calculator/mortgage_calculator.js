f4e554486a1e0aaa61fe3bf18a1088174cbd4dbb
Clara Pastore <clarapastore17@gmail.com>
2025-03-19 18:09:21 +0100

    Asks annual interest rate

--- lesson_2/mortgage_calculator/mortgage_calculator.js
+++ lesson_2/mortgage_calculator/mortgage_calculator.js
@@ -6,9 +6,14 @@
 // Ask to calculate another loan
 
 let readline = require("readline-sync");
-let loanAmount;
+let loanAmount, annualInterestRate;
 
 console.log("Welcome to Mortgage Calculator!");
+
 console.log("What's the loan amount you have to pay?");
 loanAmount = readline.question();
 console.log(`Your loan amount is ${loanAmount}`);
+
+console.log("What's the annual interest rate?");
+annualInterestRate = readline.question();
+console.log(`Your annual interest rate is ${annualInterestRate}`);
