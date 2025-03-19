356d27ebc363fef48f12b52b8405979d478d8994
Clara Pastore <clarapastore17@gmail.com>
2025-03-19 17:14:39 +0100

    Starts by getting loan duration

--- lesson_2/mortgage_calculator/mortgage_calculator.js
+++ lesson_2/mortgage_calculator/mortgage_calculator.js
@@ -4,3 +4,11 @@
 // Calculate monthly payment
 // Output monthly payment
 // Ask to calculate another loan
+
+let readline = require("readline-sync");
+let loanAmount;
+
+console.log("Welcome to Mortgage Calculator!");
+console.log("What's the loan amount you have to pay?");
+loanAmount = readline.question();
+console.log(`Your loan amount is ${loanAmount}`);
