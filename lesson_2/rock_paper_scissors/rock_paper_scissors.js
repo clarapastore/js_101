ac3e53e47d5b06d87eb413439d66dc8a536eb79d
Clara Pastore <clarapastore17@gmail.com>
2025-03-26 14:33:10 +0100

    Asks user to choose option

--- lesson_2/rock_paper_scissors/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.js
@@ -3,3 +3,11 @@ const readline = require("readline-sync");
 function prompt(message) {
   console.log(`=> ${message}`);
 }
+
+prompt("Choose one: rock, paper, scissors");
+let choice = readline.question();
+
+while (!["rock", "paper", "scissors"].includes(choice)) {
+  prompt("That's not a valid choice");
+  choice = readline.question();
+}
