2a2f5d1d3dadea9c023a53a379711505dad5ecad
Clara Pastore <clarapastore17@gmail.com>
2025-03-26 16:59:29 +0100

    Adds looping logic to allow for more rounds

--- lesson_2/rock_paper_scissors/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.js
@@ -9,31 +9,41 @@ function chooseRandomIndex() {
   return Math.floor(Math.random() * VALID_CHOICES.length);
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
-let choice = readline.question();
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+  let choice = readline.question();
 
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice");
-  choice = readline.question();
-}
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
 
-let computerChoice = VALID_CHOICES[chooseRandomIndex()];
-prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+  let computerChoice = VALID_CHOICES[chooseRandomIndex()];
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-// deciding the winner based on the conditions
-// Writing the conditional for the case the user wins:
-if (
-  (choice === "rock" && computerChoice === "scissors") ||
-  (choice === "paper" && computerChoice === "rock") ||
-  (choice === "scissors" && computerChoice === "paper")
-) {
-  prompt("You win!");
-} else if (
-  (choice === "rock" && computerChoice === "paper") ||
-  (choice === "paper" && computerChoice === "scissors") ||
-  (choice === "scissors" && computerChoice === "rock")
-) {
-  prompt("Computer wins!");
-} else {
-  prompt("It's a tie!");
+  if (
+    (choice === "rock" && computerChoice === "scissors") ||
+    (choice === "paper" && computerChoice === "rock") ||
+    (choice === "scissors" && computerChoice === "paper")
+  ) {
+    prompt("You win!");
+  } else if (
+    (choice === "rock" && computerChoice === "paper") ||
+    (choice === "paper" && computerChoice === "scissors") ||
+    (choice === "scissors" && computerChoice === "rock")
+  ) {
+    prompt("Computer wins!");
+  } else {
+    prompt("It's a tie!");
+  }
+
+  prompt("Do you want to play another game? (y/n)");
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== "y" && answer[0] !== "n") {
+    prompt("This is not a valid answer. Please enter y or n.");
+    answer = readline.question().toLowerCase();
+  }
+  if (answer[0] !== "y") {
+    break;
+  }
 }
