9d5e735e6daa4cb514be5e2ec29b482d42844c8c
Clara Pastore <clarapastore17@gmail.com>
2025-03-26 17:01:49 +0100

    Extracts winner logic to separate function

--- lesson_2/rock_paper_scissors/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.js
@@ -9,18 +9,7 @@ function chooseRandomIndex() {
   return Math.floor(Math.random() * VALID_CHOICES.length);
 }
 
-while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
-  let choice = readline.question();
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
-  }
-
-  let computerChoice = VALID_CHOICES[chooseRandomIndex()];
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-
+function displayWinner(choice, computerChoice) {
   if (
     (choice === "rock" && computerChoice === "scissors") ||
     (choice === "paper" && computerChoice === "rock") ||
@@ -36,6 +25,21 @@ while (true) {
   } else {
     prompt("It's a tie!");
   }
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let computerChoice = VALID_CHOICES[chooseRandomIndex()];
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  displayWinner(choice, computerChoice);
 
   prompt("Do you want to play another game? (y/n)");
   let answer = readline.question().toLowerCase();
