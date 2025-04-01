f99447624879585bd86ae1cef9667cca05375f56
Clara Pastore <clarapastore17@gmail.com>
2025-04-01 15:42:42 +0200

    Adds solution for exercise 3 medium 1

---
+++ lesson_3/medium_1_exercise_3.js
@@ -0,0 +1,17 @@
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  }
+  return factors;
+}
+
+console.log(factors(0));
+console.log(factors(-1));
+console.log(factors(1));
+console.log(factors(2));
+console.log(factors(3));
