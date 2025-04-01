e4aa74453737d6445dc672cdfb14208f7fd24877
Clara Pastore <clarapastore17@gmail.com>
2025-04-01 16:33:39 +0200

    Completes exercise 4 of medium 1

---
+++ lesson_3/medium_1_exercise_4.js
@@ -0,0 +1,20 @@
+function addToRollingBuffer1(buffer, maxBufferSize, newElement) {
+  buffer.push(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+
+// function addToRollingBuffer1 mutates the array passed to it as an argument
+
+function addToRollingBuffer2(buffer, maxBufferSize, newElement) {
+  buffer = buffer.concat(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+
+// function addToRollingBuffer2 doesn't mutate the array passed
+//  to it as an argument
